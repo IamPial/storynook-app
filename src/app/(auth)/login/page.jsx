44fdcce578f95bf74aca5c199c9109a36206b90e
Pial <pialname394@gmail.com>
2026-05-20 16:26:20 +0600

    create login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,73 +2,76 @@
 
 import {
   Button,
-  Description,
   FieldError,
   Form,
   Input,
   Label,
+  Separator,
   TextField,
 } from "@heroui/react";
-import { FaCheck } from "react-icons/fa";
+import Image from "next/image";
+import bookIcon from "@/assets/book.png";
+import { FcGoogle } from "react-icons/fc";
+import Link from "next/link";
 
 const LoginPage = () => {
-  //   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //     e.preventDefault();
-  //     const formData = new FormData(e.currentTarget);
-  //     const data: Record<string, string> = {};
-
-  //     // Convert FormData to plain object
-  //     formData.forEach((value, key) => {
-  //       data[key] = value.toString();
-  //     });
-
-  //     alert(`Form submitted with: ${JSON.stringify(data, null, 2)}`);
-  //   };
-
   return (
-    <div>
-      <Form className="flex w-96 flex-col gap-4 border p-4">
+    <div className="py-10  px-4 md:px-0 ">
+      <Form className="rounded-lg bg-background/70 shadow-lg flex border border-purple-100 p-5  max-w-md mx-auto flex-col gap-4">
+        <div>
+          <Image
+            src={bookIcon}
+            alt="bookImage"
+            width={50}
+            height={50}
+            className="mx-auto my-2 border border-purple-200 bg-white rounded-lg"
+          />
+          <h2 className="text-center text-3xl">Login to StudyNook</h2>
+          <p className="text-center text-purple-500 my-2 text-lg">
+            Welcome back. Pick up where you left off
+          </p>
+        </div>
         <TextField isRequired name="email" type="email">
           <Label>Email</Label>
-          <Input placeholder="john@example.com" />
+          <Input
+            className="rounded-lg  focus:right-2 focus:ring-purple-400  border border-purple-200 w-full shadow-none  mt-0.5"
+            placeholder="john@example.com"
+          />
           <FieldError />
         </TextField>
 
-        <TextField
-          isRequired
-          minLength={8}
-          name="password"
-          type="password"
-          validate={(value) => {
-            if (value.length < 8) {
-              return "Password must be at least 8 characters";
-            }
-            if (!/[A-Z]/.test(value)) {
-              return "Password must contain at least one uppercase letter";
-            }
-            if (!/[0-9]/.test(value)) {
-              return "Password must contain at least one number";
-            }
-
-            return null;
-          }}
-        >
+        <TextField isRequired minLength={6} name="password" type="password">
           <Label>Password</Label>
-          <Input placeholder="Enter your password" />
-          <Description>
-            Must be at least 8 characters with 1 uppercase and 1 number
-          </Description>
+          <Input
+            className="rounded-lg focus:right-2 focus:ring-purple-400   border border-purple-200 w-full shadow-none  mt-0.5"
+            placeholder="create a password"
+          />
           <FieldError />
         </TextField>
 
-        <div className="flex gap-2">
-          <Button type="submit">
-            <FaCheck />
-            Submit
+        <div className="mb-2">
+          <Button
+            type="submit"
+            className="cursor-pointer w-full rounded-lg  bg-[#9d4edd] hover:bg-[#8d46c7]"
+          >
+            Login
           </Button>
-          <Button type="reset" variant="secondary">
-            Reset
+          <div className="flex gap-1 justify-between items-center ">
+            <Separator className="w-45" />
+            <p className="text-center text-gray-500 my-2 ">OR</p>
+            <Separator className="w-45" />
+          </div>
+          <Button className="w-full  hover:bg-purple-400 rounded-lg bg-white border text-neutral-900 border-gray-200 hover:text-white ">
+            <FcGoogle className="mb-2" />
+            Sign Up With Google
           </Button>
+        </div>
+        <Separator />
+        <div className="text-center text-dark">
+          Don&apos;t have an account?{" "}
+          <Link href="/signup" className="text-purple-500 hover:underline">
+            Register
+          </Link>
         </div>
       </Form>
     </div>
