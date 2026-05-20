faf0a0cb78d0150af2f2b8899887019134b31c53
Pial <pialname394@gmail.com>
2026-05-20 12:24:43 +0600

    wrap the footer components

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,11 +1,13 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
 const MainLayoutPage = ({ children }) => {
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
       {children}
+      <Footer />
     </>
   );
 };
