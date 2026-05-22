d7c987553c6e68b858cb9252261dc982dd07ce2f
Pial <pialname394@gmail.com>
2026-05-22 20:38:54 +0600

    created homepage card with limitation

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/Banner";
 import ChoosePage from "@/components/ChooseArea";
+import HomePageCard from "@/components/HomePageCard";
 import WorkPage from "@/components/Work";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <HomePageCard />
       <ChoosePage />
       <WorkPage />
     </div>
