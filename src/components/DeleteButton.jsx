75bfc5705d4e57728239b224e065c0e9126f4ef5
Pial <pialname394@gmail.com>
2026-05-26 10:58:35 +0600

    remove comments

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -31,45 +31,36 @@ const DeleteButton = ({ bookingId }) => {
   };
 
   return (
-    <>
-      {/* <Button
-        onClick={handleDeleteBtn}
-        className="rounded-lg border border-red-200 bg-purple-50 text-red-500 hover:bg-red-50"
-      >
-        Cancel
-      </Button> */}
-
-      <AlertDialog>
-        <Button className="rounded-lg border border-red-200 bg-purple-50 text-red-500 hover:bg-red-50">
-          <RiDeleteBin6Line /> Cancel
-        </Button>
-        <AlertDialog.Backdrop>
-          <AlertDialog.Container>
-            <AlertDialog.Dialog className="sm:max-w-[400px]">
-              <AlertDialog.CloseTrigger />
-              <AlertDialog.Header>
-                <AlertDialog.Icon status="danger" />
-                <AlertDialog.Heading>Cancel this booking?</AlertDialog.Heading>
-              </AlertDialog.Header>
-              <AlertDialog.Body>
-                <p>
-                  You&apos;ll lose your reserved time slot. The room will become
-                  available to others.
-                </p>
-              </AlertDialog.Body>
-              <AlertDialog.Footer>
-                <Button slot="close" variant="tertiary">
-                  Keep Booking
-                </Button>
-                <Button slot="close" variant="danger" onClick={handleDeleteBtn}>
-                  Yes,Cancel
-                </Button>
-              </AlertDialog.Footer>
-            </AlertDialog.Dialog>
-          </AlertDialog.Container>
-        </AlertDialog.Backdrop>
-      </AlertDialog>
-    </>
+    <AlertDialog>
+      <Button className="rounded-lg border border-red-200 bg-purple-50 text-red-500 hover:bg-red-50">
+        <RiDeleteBin6Line /> Cancel
+      </Button>
+      <AlertDialog.Backdrop>
+        <AlertDialog.Container>
+          <AlertDialog.Dialog className="sm:max-w-[400px]">
+            <AlertDialog.CloseTrigger />
+            <AlertDialog.Header>
+              <AlertDialog.Icon status="danger" />
+              <AlertDialog.Heading>Cancel this booking?</AlertDialog.Heading>
+            </AlertDialog.Header>
+            <AlertDialog.Body>
+              <p>
+                You&apos;ll lose your reserved time slot. The room will become
+                available to others.
+              </p>
+            </AlertDialog.Body>
+            <AlertDialog.Footer>
+              <Button slot="close" variant="tertiary">
+                Keep Booking
+              </Button>
+              <Button slot="close" variant="danger" onClick={handleDeleteBtn}>
+                Yes,Cancel
+              </Button>
+            </AlertDialog.Footer>
+          </AlertDialog.Dialog>
+        </AlertDialog.Container>
+      </AlertDialog.Backdrop>
+    </AlertDialog>
   );
 };
 
